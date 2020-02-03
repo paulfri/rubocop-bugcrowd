9bdc6d06ced0bf343d1ca4b0163677f3c8217607
Max Schwenk <maschwenk@gmail.com>
2020-02-03 14:23:32 -0800

    Add RailsConfigurationMutation cop

--- lib/rubocop/cop/bugcrowd_cops.rb
+++ lib/rubocop/cop/bugcrowd_cops.rb
@@ -2,6 +2,7 @@
 
 require_relative 'bugcrowd/database'
 require_relative 'bugcrowd/faker'
+require_relative 'bugcrowd/rails_configuration_mutation'
 
 require_relative 'bugcrowd/prefer_text_to_string_column'
 require_relative 'bugcrowd/uuid_primary_keys'
